f1c9e24e794a2273b268b7c6cf80e0a647ba17df
ipwx <jw987747@gmail.com>
2023-10-20 14:21:30 +0800

    TN=2 succeeded

--- OurModels/ModelImprovement/GRU.py
+++ OurModels/ModelImprovement/GRU.py
@@ -20,96 +20,127 @@ from util import expo, log, filter_by_date
 
 
 # 模型定义
-class GRUModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, dropout):
-        super(GRUModel, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.gru = nn.GRU(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True, bidirectional=False)
-        self.fc = nn.Linear(hidden_size, 1)  # 假设输出是一个数值
+class MLPModel(nn.Module):
+    def __init__(self, input_size=158, output_size=1):
+        super(MLPModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(64,32)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(32, output_size)
 
-    def forward(self, x, hidden=None):
-        batch_size = x.shape[0]  # 获取批次大小
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu2(out)
+        out = self.fc3(out)
+        return out
 
-        # 初始化隐层状态
-        if hidden is None:
-            h_0 = x.data.new(self.num_layers, batch_size, self.hidden_size).fill_(0).float()
-        else:
-            h_0 = hidden
+class GRU(nn.Module):
+    def __init__(self, input_size=158, hidden_size=64, target_size=1):
+        super(GRU, self).__init__()
+        self.gru = nn.GRU(
+            input_size=input_size,# 传入我们上面定义的参数
+            hidden_size=hidden_size,# 传入我们上面定义的参数
+            batch_first=True,# 为什么设置为True上面解释过了
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_size, 32), # 加入线性层的原因是，GRU的输出，参考官网为(batch_size, seq_len, hidden_size)
+            nn.LeakyReLU(),             # 这边的多层全连接，根据自己的输出自己定义就好，
+            nn.Linear(32, 16),          # 我们需要将其最后打成（batch_size, output_size）比如单值预测，这个output_size就是1，
+            nn.LeakyReLU(),             # 这边我们等于targets
+            nn.Linear(16, target_size)      # 这边输出的（batch_size, targets）且这个targets是上面一个模块已经定义好了
+        )
 
-        # GRU运算
-        output, h_0 = self.gru(x, h_0)
+    def forward(self, input):
+        output, h_n = self.gru(input, None)# output:(batch_size, seq_len, hidden_size)，h0可以直接None
+        # print(output.shape)
+        output = output[:, -1, :]# output:(batch_size, hidden_size)
+        output = self.mlp(output)# 进过一个多层感知机，也就是全连接层，output:(batch_size, output_size)
+        return output
 
-        # 获取GRU输出的维度信息
-        batch_size, timestep, hidden_size = output.shape
+class LSTM(nn.Module):
+    def __init__(self, input_size=158, hidden_size=64, target_size=1, drop_out=0.15):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_size,  # 传入我们上面定义的参数
+            hidden_size=hidden_size,  # 传入我们上面定义的参数
+            batch_first=True,  # 为什么设置为True上面解释过了
+            dropout=drop_out
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_size, 32), # 加入线性层的原因是，GRU的输出，参考官网为(batch_size, seq_len, hidden_size)
+            nn.LeakyReLU(),             # 这边的多层全连接，根据自己的输出自己定义就好，
+            nn.Linear(32, 16),          # 我们需要将其最后打成（batch_size, output_size）比如单值预测，这个output_size就是1，
+            nn.LeakyReLU(),             # 这边我们等于targets
+            nn.Linear(16, target_size)      # 这边输出的（batch_size, targets）且这个targets是上面一个模块已经定义好了
+        )
 
-        # 将output变成 batch_size * timestep, hidden_dim
-        output = output.reshape(-1, hidden_size)
-
-        # 全连接层
-        output = self.fc(output)  # 形状为batch_size * timestep, 1
-
-        # 转换维度，用于输出
-        output = output.reshape(timestep, batch_size, -1)
-
-        # 我们只需要返回最后一个时间片的数据即可
-        return output[-1]
+    def forward(self, input):
+        output, h_n = self.lstm(input, None)  # output:(batch_size, seq_len, hidden_size)，h0可以直接None
+        # print(output.shape)
+        output = output[:, -1, :]  # output:(batch_size, hidden_size)
+        output = self.mlp(output)  # 进过一个多层感知机，也就是全连接层，output:(batch_size, output_size)
+        return output
 
 
 if __name__ == '__main__':
     # 一些训练参数：
-    class config:
+    class TrainConfig:
         TN = 2  # 1代表只用当期数据，2代表用当期和前一期数据，以此类推
         train_begin_year = 2009  # 从2009开始滚动
         training_time_span = 5  # 用于训练的年份数
         valid_time_span = 0  # 用于验证的年份数
         batch_size = 256
         input_size = 158  # 模型输入向量维度，即每个截面的因子数量
-        hidden_size = 64  # 模型隐藏层大小
-        num_layers = 2  # gru隐藏层层数
-        dropout = 0.2  # 模型训练参数，dropout rate
-        num_epochs = 1  # 训练epoch数
+        num_epochs = 10  # 训练epoch数
         base_path = 'C:\\Users\\ipwx\\Desktop\\testing\\'
-        rolling_window = 1  # 滚动次数，至少是1
+        rolling_window = 2022 - train_begin_year - training_time_span - valid_time_span + 1  # 滚动次数，至少是1
 
+    class ModelConfig:
+        input_size = 158
+        hidden_size = 64
+        num_layers = 2
+        dropout = 0.15  # 模型训练参数，dropout rate
 
     # 数据准备
-    data: pd.DataFrame = pd.read_hdf(config.base_path + "_Alpha158_Financial01_Barra_HT_proceed.hdf")
+    data: pd.DataFrame = pd.read_hdf(TrainConfig.base_path + "_Alpha158_Financial01_Barra_HT_proceed.hdf")
     dates = data.index.get_level_values('date')
     start_date, end_date = dates.min(), dates.max()
     pred_data = pd.Series(index=data.index, name='PRED_NEXT_RET')
 
-    for year in range(config.train_begin_year, config.train_begin_year + config.rolling_window):
+    for year in range(TrainConfig.train_begin_year, TrainConfig.train_begin_year + TrainConfig.rolling_window):
         # 定义滚动窗口
-        train_year_list = [year + i for i in range(config.training_time_span)]
-        valid_year_list = [year + config.training_time_span + i for i in range(config.valid_time_span)]
-        test_year_list = [year + config.training_time_span + config.valid_time_span]  # 测试只用1年的数据
-        train_X, train_y, _ = load_snippets(train_year_list, config.base_path, config.TN)
-        valid_X, valid_y, _ = load_snippets(valid_year_list, config.base_path, config.TN)
-        test_X, test_y, index_info = load_snippets(test_year_list, config.base_path, config.TN)
+        train_year_list = [year + i for i in range(TrainConfig.training_time_span)]
+        valid_year_list = [year + TrainConfig.training_time_span + i for i in range(TrainConfig.valid_time_span)]
+        test_year_list = [year + TrainConfig.training_time_span + TrainConfig.valid_time_span]  # 测试只用1年的数据
+        train_X, train_y, _ = load_snippets(train_year_list, TrainConfig.base_path, TrainConfig.TN)
+        valid_X, valid_y, _ = load_snippets(valid_year_list, TrainConfig.base_path, TrainConfig.TN)
+        test_X, test_y, index_info = load_snippets(test_year_list, TrainConfig.base_path, TrainConfig.TN)
 
         train_dataset = TensorDataset(train_X, train_y)
         valid_dataset = TensorDataset(valid_X, valid_y)
         test_dataset = TensorDataset(test_X, test_y)
-        train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
-        if config.valid_time_span > 0:
-            valid_loader = DataLoader(valid_dataset, batch_size=config.batch_size, shuffle=False)
-        test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=TrainConfig.batch_size, shuffle=True)
+        if TrainConfig.valid_time_span > 0:
+            valid_loader = DataLoader(valid_dataset, batch_size=TrainConfig.batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=TrainConfig.batch_size, shuffle=True)
 
-        model = GRUModel(config.input_size, config.hidden_size, config.num_layers, config.dropout)
+        model = LSTM(ModelConfig.input_size, ModelConfig.hidden_size, 1, ModelConfig.dropout)
         criterion = nn.MSELoss()
         criterion2 = nn.L1Loss()
         optimizer = Adam(model.parameters(), lr=0.01)  # 初始化时的学习率设置为0.01
 
         # 定义当达到第5和10个epoch时降低学习率
-        scheduler = MultiStepLR(optimizer, milestones=[int(config.num_epochs / 2), int(config.num_epochs / 4 * 3)], gamma=0.1)
+        scheduler = MultiStepLR(optimizer, milestones=[int(TrainConfig.num_epochs / 2), int(TrainConfig.num_epochs / 4 * 3)], gamma=0.1)
 
         # 开始统计模型运行时间
         start_time = time.time()
 
         logger.info(f"Training model for {year}...")
 
-        for epoch in range(config.num_epochs):
+        for epoch in range(TrainConfig.num_epochs):
             training_loss = 0
             model.train()
             for batch_x, batch_y in tqdm(train_loader, desc=f'Epoch {epoch + 1} train'):
@@ -122,7 +153,7 @@ if __name__ == '__main__':
             print(f"Training Loss: {training_loss / len(train_loader):.6f}")
 
             # 在验证集上进行测试
-            if config.valid_time_span > 0:
+            if TrainConfig.valid_time_span > 0:
                 model.eval()
                 valid_loss = 0
                 with torch.no_grad():
@@ -131,7 +162,7 @@ if __name__ == '__main__':
                         outputs = model(batch_x)
                         loss = criterion(outputs, batch_y.reshape(-1, 1))
                         valid_loss += loss.item()
-                    print(f"Validation Loss: {valid_loss / len(valid_X) * config.batch_size:.6f}")
+                    print(f"Validation Loss: {valid_loss / len(valid_X) * TrainConfig.batch_size:.6f}")
             scheduler.step()
 
         end_time = time.time()
@@ -174,8 +205,8 @@ if __name__ == '__main__':
     merged_data = pd.concat([data.loc[:, columns_to_keep], pred_data], axis=1, join='inner')
 
     # 对merged_data和pred_data进行日期过滤
-    test_begin_year = config.train_begin_year + config.training_time_span + config.valid_time_span
-    test_end_year = test_begin_year + config.rolling_window - 1
+    test_begin_year = TrainConfig.train_begin_year + TrainConfig.training_time_span + TrainConfig.valid_time_span
+    test_end_year = test_begin_year + TrainConfig.rolling_window - 1
     start_date_filter = datetime(test_begin_year, 1, 1).date()
     end_date_filter = datetime(test_end_year, 12, 31).date()
     merged_data = filter_by_date(merged_data, start_date_filter, end_date_filter)
